b1d236f39b280590e5667e89d36a2001bfae704e
Anushka Srivastava <anushkas12345@gmail.com>
2023-04-13 22:42:18 +0530

    1st mapreduce

--- map.py
+++ map.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 from collections import defaultdict
 
@@ -6,16 +6,20 @@ from collections import defaultdict
 
 # Map Function
 
-if(sys.argv[1]==-1):
+if(sys.argv[1]=="-1"):
     for line in sys.stdin:
+        # print(line)
+        # continue
         # Split the line into fields
         fields = line.strip().split(',')
         # Extract pick-up and drop-off locations
-        pickup = fields[5]
-        dropoff = fields[6]
+        pickup = fields[7]
+        dropoff = fields[8]
         # Emit key-value pairs
         # yield (pickup, 1)
         # yield (dropoff, 1)
-        print(pickup, 1)
-        print(dropoff, 1)
+        with open("check.txt", "a") as f:
+            f.write(f"{pickup} {1}")
+        print(f"{pickup}\t{1}")
+        print(f"{dropoff}\t{1}")
     
--- reduce.py
+++ reduce.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 from collections import defaultdict
 
@@ -6,10 +6,34 @@ from collections import defaultdict
 
 # Reduce Function
 
-if(sys.argv[1]==-1):
-    for values, key in sys.stdin:
-        # Calculate the total number of trips for the location
-        total_trips = sum(values)
-        # Output result
-        print(total_trips, key)
+curr_loc = ""
+curr_count = 0
+
+if(sys.argv[1]=="-1"):
+    # total = 0
+    for line in sys.stdin:
+        # print(line)
+    # #     continue
+        key, values = line.strip().split('\t')
+        # total = sum(map(int, values))
+        if (key == curr_loc or curr_loc==""):
+            curr_loc = key
+            curr_count += int(values)
+        else:
+            print(curr_loc, curr_count)
+            curr_count = int(values)
+            curr_loc = key
+
+
+    print(curr_loc, curr_count)
+    
+    #     # values = float(values)
+    #     # Calculate the total number of trips for the location
+    #     total_trips = 0
+    #     for i in values:
+    #         total_trips += int(i)
+    #     # total_trips = sum(values)
+    #     # Output result
+    #     print(total_trips, key)
+
         
