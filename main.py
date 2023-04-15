aad42a981482fa017e2ea0e8d241e64a50388c67
Anushka Srivastava <anushkas12345@gmail.com>
2023-04-15 18:54:38 +0530

    main function merge

--- main.py
+++ main.py
@@ -2,71 +2,170 @@ import os
 import time
 # SELECT VendorID, tpep_pickup_datetime, RatecodeID FROM tabfj WHERE RatecodeID > 100 AND fare_amount < 10;
 
-inputDir = "/taxi/acchaData.csv"
+inputDir = "/taxi/temp.csv"
 outputFolder = "/taxi"
-queryNo = 19
+queryNo = 0
 
-cols = {'VendorID':"0",
-        'tpep_pickup_datetime':"1", 
-        'tpep_dropoff_datetime':"2",
-        'passenger_count':"3", 
-        'trip_distance':"4", 
-        'RatecodeID':"5", 
-        'store_and_fwd_flag':"6",       
-        'PULocationID':"7", 
-        'DOLocationID':"8", 
-        'payment_type':"9", 
-        'fare_amount':"10", 
-        'extra':"11",       
-        'mta_tax':"12", 
-        'tip_amount':"13", 
-        'tolls_amount':"14", 
-        'improvement_surcharge':"15",       
-        'total_amount':"16", 
-        'congestion_surcharge':"17",
-        'airport_fee':"18"
-        }
 
+cols = {'VendorID': "0",
+       'tpep_pickup_datetime': "1",
+       'tpep_dropoff_datetime': "2",
+       'passenger_count': "3",
+       'trip_distance': "4",
+       'RatecodeID': "5",
+       'store_and_fwd_flag': "6",
+       'PULocationID': "7",
+       'DOLocationID': "8",
+       'payment_type': "9",
+       'fare_amount': "10",
+       'extra': "11",
+       'mta_tax': "12",
+       'tip_amount': "13",
+       'tolls_amount': "14",
+       'improvement_surcharge': "15",
+       'total_amount': "16",
+       'congestion_surcharge': "17",
+       'airport_fee': "18"
+       }
+
+
+def select_where_query(queryNo, sel_args = None, where_args = None):
+    sel_args = "VendorID tpep_pickup_datetime"
+    where_args = "VendorID:=:2?passenger_count:=:3.0"
+
+    print("sel_args", sel_args)
+    print("where_args", where_args)
+
+    mapDir = f"\'./mapper.py 0 {where_args}\'"
+    reduceDir = f"\'./reducer.py 0 {sel_args}\'"
+    outputDir = f"{outputFolder}/output{queryNo}"
+
+    cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
+    os.system(cmd)
+    time.sleep(1)
+    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    os.system(getOutput)
+    cleanUp = f"hdfs dfs -rm -r {outputDir}"
+    os.system(cleanUp)
+
+def stats_page1(queryNo):
+
+    print("popular pickup locations")
+
+    mapDir = f"\'./mapper.py 1\'"
+    reduceDir = f"\'./reducer.py 1\'"
+    outputDir = f"{outputFolder}/output{queryNo}"
+    cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
+    os.system(cmd)
+    time.sleep(1)
+    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    os.system(getOutput)
+    cleanUp = f"hdfs dfs -rm -r {outputDir}"
+    os.system(cleanUp)
+
+def stats_page2(queryNo):
+    print("popular dropoff location")
+
+    mapDir = f"\'./mapper.py 2\'"
+    reduceDir = f"\'./reducer.py 2\'"
+    outputDir = f"{outputFolder}/output{queryNo}"
+    cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
+    os.system(cmd)
+    time.sleep(1)
+    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    os.system(getOutput)
+    cleanUp = f"hdfs dfs -rm -r {outputDir}"
+    os.system(cleanUp)
+
+def stats_page3(queryNo):
+    print("busiest hour")
+    mapDir = f"\'./mapper.py 3\'"
+    reduceDir = f"\'./reducer.py 3\'"
+    outputDir = f"{outputFolder}/output{queryNo}"
+    cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
+    os.system(cmd)
+    time.sleep(1)
+    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    os.system(getOutput)
+    cleanUp = f"hdfs dfs -rm -r {outputDir}"
+    os.system(cleanUp)
+
+def stats_page4(queryNo):
+    print("revenue vs month")
+    mapDir = f"\'./mapper.py 4\'"
+    reduceDir = f"\'./reducer.py 4\'"
+    outputDir = f"{outputFolder}/output{queryNo}"
+    cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
+    os.system(cmd)
+    time.sleep(1)
+    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    os.system(getOutput)
+    cleanUp = f"hdfs dfs -rm -r {outputDir}"
+    os.system(cleanUp)
+
+
+def stats_page5(queryNo):
+    print("revenue vs day of the week")
+    mapDir = f"\'./mapper.py 5\'"
+    reduceDir = f"\'./reducer.py 5\'"
+    outputDir = f"{outputFolder}/output{queryNo}"
+    cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
+    os.system(cmd)
+    time.sleep(1)
+    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    os.system(getOutput)
+    cleanUp = f"hdfs dfs -rm -r {outputDir}"
+    os.system(cleanUp)
+
+def stats_page6(queryNo):
+    print("busiest day of the week")
+    mapDir = f"\'./mapper.py 6\'"
+    reduceDir = f"\'./reducer.py 6\'"
+    outputDir = f"{outputFolder}/output{queryNo}"
+    cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
+    os.system(cmd)
+    time.sleep(1)
+    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    os.system(getOutput)
+    cleanUp = f"hdfs dfs -rm -r {outputDir}"
+    os.system(cleanUp)
+
+def stats_page7(queryNo):
+    print("total trips vs month")
+    mapDir = f"\'./mapper.py 7\'"
+    reduceDir = f"\'./reducer.py 7\'"
+    outputDir = f"{outputFolder}/output{queryNo}"
+    cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
+    os.system(cmd)
+    time.sleep(1)
+    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    os.system(getOutput)
+    cleanUp = f"hdfs dfs -rm -r {outputDir}"
+    os.system(cleanUp)
 
 
 while(True):
-    query = input("query: (enter 0 for select where..., 1 for other)")
-    if(query==1):
-        data = input("myMapReduce> ")
-        if data == "exit":
-            print("Bye!")
-            break
+    data = int(input("\nmyMapReduce> "))
+    if data == -1:
+       print("Bye!")
+       break
+    queryNo = queryNo + 1
+    if data == 0:
+        select_where_query(queryNo)
+    elif data == 1:
+        stats_page1(queryNo)
+    elif data == 2:
+        stats_page2(queryNo)
+    elif data == 3:
+        stats_page3(queryNo)
+    elif data == 4:
+        stats_page4(queryNo)
+    elif data == 5:
+        stats_page5(queryNo)
+    elif data == 6:
+        stats_page6(queryNo)
+    elif data == 7:
+        stats_page7(queryNo)
 
-        sel_idx = data.find("SELECT")
-        from_idx = data.find("FROM")
-        where_idx = data.find("WHERE")
+   
 
-        sel_args = data[sel_idx+7:from_idx]
-        where_args = data[where_idx+6:-1]
-
-        mapDir = f"\'./map.py {where_args}\'"
-        reduceDir = f"\'./reduce.py {sel_args}\'"
-        outputDir = f"{outputFolder}/{queryNo}"
-        queryNo+=1
-
-        cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reduce {reduceDir}"
-        # cmd = "ls"
-        os.system(cmd)
-
-        getOutput = f"hdfs dfs -cat {outputFolder}/*"
-        os.system(cmd)
-    else:
-        data = input("enter 1 for most popular pickup and dropoff")
-        if data == "exit":
-            print("Bye!")
-            break
-
-        
-        mapDir = f"\'./map.py {-1}\'"
-        reduceDir = f"\'./reduce.py {-1}\'"
-        outputDir = f"{outputFolder}/{queryNo}"
-        queryNo+=1
-
-        cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reduce {reduceDir}"
-        # cmd = "ls"
-        os.system(cmd)
--- mapper.py
+++ mapper.py
@@ -1,60 +1,53 @@
 #!/usr/bin/env python3
 
 import sys
+from collections import defaultdict
 
-cols = {'VendorID':"0",
-        'tpep_pickup_datetime':"1", 
-        'tpep_dropoff_datetime':"2",
-        'passenger_count':"3", 
-        'trip_distance':"4", 
-        'RatecodeID':"5", 
-        'store_and_fwd_flag':"6",       
-        'PULocationID':"7", 
-        'DOLocationID':"8", 
-        'payment_type':"9", 
-        'fare_amount':"10", 
-        'extra':"11",       
-        'mta_tax':"12", 
-        'tip_amount':"13", 
-        'tolls_amount':"14", 
-        'improvement_surcharge':"15",       
-        'total_amount':"16", 
-        'congestion_surcharge':"17",
-        'airport_fee':"18"
+cols = {'VendorID': "0",
+        'tpep_pickup_datetime': "1",
+        'tpep_dropoff_datetime': "2",
+        'passenger_count': "3",
+        'trip_distance': "4",
+        'RatecodeID': "5",
+        'store_and_fwd_flag': "6",
+        'PULocationID': "7",
+        'DOLocationID': "8",
+        'payment_type': "9",
+        'fare_amount': "10",
+        'extra': "11",
+        'mta_tax': "12",
+        'tip_amount': "13",
+        'tolls_amount': "14",
+        'improvement_surcharge': "15",
+        'total_amount': "16",
+        'congestion_surcharge': "17",
+        'airport_fee': "18"
         }
 
-def map(args):
+
+def map_select_where(args):
     for l in sys.stdin:
         # split all lines on comma maybe
-        if len(l)==0: 
+        if len(l) == 0:
             continue
-        
+
         line = l.strip(" ").split(",")
         take = True
-        # with open("mapper_log.txt","a") as f:
-        #     f.write(f"{l}\n")
-        #     for a in args:
-        #         f.write(str(a) + " ")
-        #     f.write("\nline:\t")
-
-        #     for a in line:
-        #         f.write(str(a) + "||||")
-        #     f.write("\n")
 
         for arg in args:
-            if arg[1]=='!':
+            if arg[1] == '!':
                 if line[arg[0]] == arg[2]:
                     take = False
                     break
-            elif arg[1]=='<':
+            elif arg[1] == '<':
                 if line[arg[0]] >= arg[2]:
                     take = False
-                    break                
-            elif arg[1]=='>':
+                    break
+            elif arg[1] == '>':
                 if line[arg[0]] <= arg[2]:
                     take = False
                     break
-            elif arg[1]=='=':
+            elif arg[1] == '=':
                 if line[arg[0]] != arg[2]:
                     take = False
                     break
@@ -62,22 +55,78 @@ def map(args):
             key = line[0]
             val = l
             print(f"{key}\t{val}")
-            with open("mapper_log.txt","a") as f:
+            with open("mapper_log.txt", "a") as f:
                 f.write(f"{key}\t{val}->Taken\n")
-            
-        
 
 
-arguments = (sys.argv[1]).split('?')
-for idx,a in enumerate(arguments):
-    a = list(a.split(':'))
-    a[0] = int(cols[a[0]])
-    # with open('mapper_log.txt','a') as f:
-    #     for a1 in a:
-    #         f.write(str(a1) + " ")
-    #     f.write("\n")
-    arguments[idx] = a
+if (sys.argv[1] == "0"):
+    arguments = (sys.argv[2]).split('?')
+    for idx, a in enumerate(arguments):
+        a = list(a.split(':'))
+        a[0] = int(cols[a[0]])
+        arguments[idx] = a
+    map_select_where(args=arguments)
 
+elif (sys.argv[1] == "1"):  # popular pickup locations
+    for line in sys.stdin:
+        # Split the line into fields
+        fields = line.strip().split(',')
+        # Extract pick-up ocations
+        pickup = fields[7]
+        # Emit key-value pairs
+        with open("check.txt", "a") as f:
+            f.write(f"{pickup} {1}")
+        print(f"{pickup}\t{1}")
 
+elif (sys.argv[1] == "2"):  # popular dropoff location
+    for line in sys.stdin:
+        # Split the line into fields
+        fields = line.strip().split(',')
+        # Extract drop-off locations
+        dropoff = fields[8]
+        # Emit key-value pairs
+        print(f"{dropoff}\t{1}")
 
-map(args = arguments)
+elif (sys.argv[1] == "3"):  # busiest hour
+    for line in sys.stdin:
+        fields = line.strip().split(',')
+        pickup_datetime = fields[1]
+        pickup_hour = pickup_datetime.split(':')[0][-2:]
+        print(f"{pickup_hour}\t1")
+
+elif (sys.argv[1] == "4"):  # revenue vs month
+    for line in sys.stdin:
+        fields = line.strip().split(',')
+        pickup_datetime = fields[1]
+        pickup_month = pickup_datetime.split('-')
+        if len(pickup_month) > 1:
+            month = pickup_month[1]
+            revenue = float(fields[16])
+            print(f"{month}\t{revenue}")
+
+elif (sys.argv[1] == "5"):  # revenue vs day of the week
+    for line in sys.stdin:
+        fields = line.strip().split(',')
+        pickup_datetime = fields[1]
+        pickup_day = pickup_datetime.split()[0]
+        if fields[16] == "total_amount":
+            continue
+        revenue = float(fields[16])
+        print(f"{pickup_day}\t{revenue}")
+
+elif (sys.argv[1] == "6"):  # busiest day of the week
+    for line in sys.stdin:
+        fields = line.strip().split(',')
+        pickup_datetime = fields[1]
+        pickup_day = pickup_datetime.split()[0]
+        print(f"{pickup_day}\t1")
+
+elif (sys.argv[1] == "7"):  # total trips vs month
+    for line in sys.stdin:
+        fields = line.strip().split(',')
+        pickup_datetime = fields[1]
+        pickup_month = pickup_datetime.split('-')
+        if len(pickup_month) <= 1:
+            continue
+        month = pickup_month[1]
+        print(f"{month}\t1")
