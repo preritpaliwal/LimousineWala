3d74249a3ee0877e207587d676a4a66e3ecff807
Anushka Srivastava <anushkas12345@gmail.com>
2023-04-15 19:28:19 +0530

    redirecting and processing output

--- main.py
+++ main.py
@@ -28,6 +28,22 @@ cols = {'VendorID': "0",
        'airport_fee': "18"
        }
 
+output_filename = "tem.txt"
+with open(output_filename, "w") as f:
+        pass
+
+def process_output(output_filename):
+    output = []
+    with open(output_filename, 'r') as f:
+        for line in f:
+            row = line.strip().split(',')
+            output.append(row)
+            print(row)
+
+    if os.path.exists(output_filename):
+        os.remove(output_filename)
+    return output
+
 
 def select_where_query(queryNo, sel_args = None, where_args = None):
     sel_args = "VendorID tpep_pickup_datetime"
@@ -40,13 +56,18 @@ def select_where_query(queryNo, sel_args = None, where_args = None):
     reduceDir = f"\'./reducer.py 0 {sel_args}\'"
     outputDir = f"{outputFolder}/output{queryNo}"
 
+    output_filename = "0.txt"
+    with open(output_filename, "w") as f:
+        pass
+
     cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
     os.system(cmd)
     time.sleep(1)
-    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    getOutput = f"hdfs dfs -cat {outputDir}/* > {output_filename}"
     os.system(getOutput)
     cleanUp = f"hdfs dfs -rm -r {outputDir}"
     os.system(cleanUp)
+    process_output(output_filename)
 
 def stats_page1(queryNo):
 
@@ -55,13 +76,20 @@ def stats_page1(queryNo):
     mapDir = f"\'./mapper.py 1\'"
     reduceDir = f"\'./reducer.py 1\'"
     outputDir = f"{outputFolder}/output{queryNo}"
+
+    output_filename = "1.txt"
+    with open(output_filename, "w") as f:
+        pass
+        
     cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
     os.system(cmd)
     time.sleep(1)
-    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    getOutput = f"hdfs dfs -cat {outputDir}/* > {output_filename}"
     os.system(getOutput)
     cleanUp = f"hdfs dfs -rm -r {outputDir}"
     os.system(cleanUp)
+    final_output = process_output(output_filename)
+    print(final_output)
 
 def stats_page2(queryNo):
     print("popular dropoff location")
@@ -69,39 +97,60 @@ def stats_page2(queryNo):
     mapDir = f"\'./mapper.py 2\'"
     reduceDir = f"\'./reducer.py 2\'"
     outputDir = f"{outputFolder}/output{queryNo}"
+
+    output_filename = "2.txt"
+    with open(output_filename, "w") as f:
+        pass
+        
     cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
     os.system(cmd)
     time.sleep(1)
-    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    getOutput = f"hdfs dfs -cat {outputDir}/* > {output_filename}"
     os.system(getOutput)
     cleanUp = f"hdfs dfs -rm -r {outputDir}"
     os.system(cleanUp)
+    final_output = process_output(output_filename)
+    print(final_output)
 
 def stats_page3(queryNo):
     print("busiest hour")
     mapDir = f"\'./mapper.py 3\'"
     reduceDir = f"\'./reducer.py 3\'"
     outputDir = f"{outputFolder}/output{queryNo}"
+
+    output_filename = "3.txt"
+    with open(output_filename, "w") as f:
+        pass
+        
     cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
     os.system(cmd)
     time.sleep(1)
-    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    getOutput = f"hdfs dfs -cat {outputDir}/* > {output_filename}"
     os.system(getOutput)
     cleanUp = f"hdfs dfs -rm -r {outputDir}"
     os.system(cleanUp)
+    final_output = process_output(output_filename)
+    print(final_output)
 
 def stats_page4(queryNo):
     print("revenue vs month")
     mapDir = f"\'./mapper.py 4\'"
     reduceDir = f"\'./reducer.py 4\'"
     outputDir = f"{outputFolder}/output{queryNo}"
+
+    output_filename = "4.txt"
+    with open(output_filename, "w") as f:
+        pass
+        
     cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
     os.system(cmd)
     time.sleep(1)
-    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    getOutput = f"hdfs dfs -cat {outputDir}/* > {output_filename}"
     os.system(getOutput)
     cleanUp = f"hdfs dfs -rm -r {outputDir}"
     os.system(cleanUp)
+    final_output = process_output(output_filename)
+    print(final_output)
 
 
 def stats_page5(queryNo):
@@ -109,39 +158,60 @@ def stats_page5(queryNo):
     mapDir = f"\'./mapper.py 5\'"
     reduceDir = f"\'./reducer.py 5\'"
     outputDir = f"{outputFolder}/output{queryNo}"
+
+    output_filename = "5.txt"
+    with open(output_filename, "w") as f:
+        pass
+        
     cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
     os.system(cmd)
     time.sleep(1)
-    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    getOutput = f"hdfs dfs -cat {outputDir}/* > {output_filename}"
     os.system(getOutput)
     cleanUp = f"hdfs dfs -rm -r {outputDir}"
     os.system(cleanUp)
+    final_output = process_output(output_filename)
+    print(final_output)
 
 def stats_page6(queryNo):
     print("busiest day of the week")
     mapDir = f"\'./mapper.py 6\'"
     reduceDir = f"\'./reducer.py 6\'"
     outputDir = f"{outputFolder}/output{queryNo}"
+
+    output_filename = "6.txt"
+    with open(output_filename, "w") as f:
+        pass
+        
     cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
     os.system(cmd)
     time.sleep(1)
-    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    getOutput = f"hdfs dfs -cat {outputDir}/* > {output_filename}"
     os.system(getOutput)
     cleanUp = f"hdfs dfs -rm -r {outputDir}"
     os.system(cleanUp)
+    final_output = process_output(output_filename)
+    print(final_output)
 
 def stats_page7(queryNo):
     print("total trips vs month")
     mapDir = f"\'./mapper.py 7\'"
     reduceDir = f"\'./reducer.py 7\'"
     outputDir = f"{outputFolder}/output{queryNo}"
+
+    output_filename = "7.txt"
+    with open(output_filename, "w") as f:
+        pass
+        
     cmd = f"hadoop jar ./hadoop-streaming-3.3.4.jar -input {inputDir} -output {outputDir} -mapper {mapDir} -reducer {reduceDir}"
     os.system(cmd)
     time.sleep(1)
-    getOutput = f"hdfs dfs -cat {outputDir}/*"
+    getOutput = f"hdfs dfs -cat {outputDir}/* > {output_filename}"
     os.system(getOutput)
     cleanUp = f"hdfs dfs -rm -r {outputDir}"
     os.system(cleanUp)
+    final_output = process_output(output_filename)
+    print(final_output)
 
 
 while(True):
@@ -166,6 +236,4 @@ while(True):
         stats_page6(queryNo)
     elif data == 7:
         stats_page7(queryNo)
-
-   
-
+    
